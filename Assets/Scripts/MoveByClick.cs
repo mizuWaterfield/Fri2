36b161020654c39c6535f424563eece3c6913241
kden <shoki0417@gmail.com>
2017-12-16 17:44:17 +0900

    エフェクト追加等
    * 俯瞰カメラとその操作スクリプトを追加
    * アセットからbloomとDoFをを追加

---
+++ Assets/Scripts/MoveByClick.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+
+// 中クリックで移動(Z,X軸方向)
+public class MoveByClick : MonoBehaviour {
+    [SerializeField, Range(0.01f, 1.0f)]
+    public float moveAmpl = 0.1f;
+
+    private Vector2 origMousePos;
+    private Vector3 origCamPos;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // 中ボタンが押されたとき、そこをカメラとマウス原点とする
+        if (Input.GetMouseButtonDown(2))
+        {
+            origMousePos = Input.mousePosition;
+            origCamPos = transform.position;
+        }
+
+        // 中ボタンが押されている間、マウスの原点との差をカメラの原点に足す
+        if(Input.GetMouseButton(2))
+        {
+            Vector2 nowMousePos = Input.mousePosition;
+            Vector2 dMousePos = (origMousePos - nowMousePos);
+            transform.position = origCamPos + moveAmpl * new Vector3(dMousePos.x, 0.0f, dMousePos.y);
+        }
+    }
+}
---
+++ Assets/Scripts/Zoom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour {
+    [SerializeField]
+    public float zoomAmpl = 50.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 Y = new Vector3(0f, 1f, 0f);
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        transform.position += Y * scroll * zoomAmpl;
+    }
+}
